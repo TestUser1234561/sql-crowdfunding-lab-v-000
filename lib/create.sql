4e3a5ebdf1b35e05bbf51925b68e402444cb86fb
Morgan Fudge <mfudge92@gmail.com>
2017-12-07 14:53:23 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE projects (
+  id INT PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INT,
+  start_date DATE,
+  end_date DATE,
+);
+
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  name TEXT,
+  age INT
+);
+
+CREATE TABLE pledges (
+  id INT PRIMARY KEY,
+  ammount INT,
+  user_id INT,
+  project_id INT
+);
